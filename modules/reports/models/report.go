679bc7ab6195fec3565938778fbe94936a0084f8
dany_arie <dany_arie@yahoo.com>
2020-02-22 16:03:43 +0700

    add struct Sallary , Val

--- modules/reports/models/report.go
+++ modules/reports/models/report.go
@@ -1,19 +1,41 @@
 package models
 
+
+// import EmplModels "smart-hr/modules/employees/models"
+
+
 type ReportBPJS struct {
-	ID 			int 	`json:"trx_id"`
-	EmployeeID  int     `json:"employee_id"`
-	BpjsID 		int 	`json:"bpjs_id"`
-	Year 		int 	`json:"year"`
-	Month 		int 	`json:"month"`
-	Sallary 	int 	`json:"sallary"`
-	Premi 		int 	`json:"premi"`
-	NIK 		string 	`json:"nik"`
-	FirstName 	string 	`json:"first_name"`
-	LastName 	string 	`json:"last_name"`
-	OfficeName 	string 	`json:"office_name"`
-	Age 		int 	`json:"age"`
+	ID         int    `json:"trx_id"`
+	EmployeeID int    `json:"employee_id"`
+	BpjsID     int    `json:"bpjs_id"`
+	Year       int    `json:"year"`
+	Month      int    `json:"month"`
+	Sallary    int    `json:"sallary"`
+	Premi      int    `json:"premi"`
+	NIK        string `json:"nik"`
+	FirstName  string `json:"first_name"`
+	LastName   string `json:"last_name"`
+	OfficeName string `json:"office_name"`
+	Age        int    `json:"age"`
 }
 
+type Sallary struct {
+	Periode        string  					`json:"periode"`
+	Code           string  					`json:"code"`
+	Ammount        float64 					`json:"ammount"`
+	Component      string  					`json:"component"`
+	Category       string  					`json:"category"`
+	Kind           string  					`json:"kind"`
+	TotalAllowance float64 					`json:"total_allowance"`
+	TotalDeduction float64 					`json:"total_deduction"`
+	TakeHomePay    float64 					`json:"take_home_pay"`
+}
+
+
+
+type Val struct {
+	ID 		int `json:"id"`
+	Ammount int `json:"ammount"`
+}
 
 //transaction_id	bpjs_trx_id	"year"	"month"	sallary	premi	nik	first_name	last_name	office_name	age
