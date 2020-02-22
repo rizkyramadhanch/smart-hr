f804aeb266568ebfc789b8fe22b6c95e5e677c08
dany_arie <dany_arie@yahoo.com>
2020-02-22 16:07:46 +0700

    fix Employee struct

--- modules/employees/models/employee.go
+++ modules/employees/models/employee.go
@@ -1,19 +1,22 @@
 package models
 
+import ComponentModel "smart-hr/modules/components/models"
+
 type Employee struct {
-	ID 				int 	`json:"id"`
-	EmployeeID 		string 	`json:"employee_id"`
-	NIK				string	`json:"nik"`
-	BpjsID			string	`json:"bpjs_id"`
-	JoinDate 		string 	`json:"join_date"`
-	FirstName 		string 	`json:"firstname"`
-	LastName 		string 	`json:"lastname"`
-	PlaceOfBirth 	string 	`json:"place_of_birth"`
-	Birthday 		string 	`json:"birthday"`
-	Address 		string 	`json:"address"`
-	Age  			int 	
-	Sallary  		int 	
+	ID 				int 			`json:"id"`
+	EmployeeID 		string 			`json:"employee_id"`
+	NIK				string			`json:"nik"`
+	BpjsID			string			`json:"bpjs_id"`
+	JoinDate 		string 			`json:"join_date"`
+	FirstName 		string 			`json:"firstname"`
+	LastName 		string 			`json:"lastname"`
+	PlaceOfBirth 	string 			`json:"place_of_birth"`
+	Birthday 		string 			`json:"birthday"`
+	Address 		string 			`json:"address"`
+	Age  			int 			`json:"age"`
+	Sallary 		int				`json:"sallary"`
 	DepartmentID    int     
 	LevelID    		int     
-	CompanyID    	int     
+	CompanyID    	int  
+	Component		[]ComponentModel.Component  `json:"component"`
 }
